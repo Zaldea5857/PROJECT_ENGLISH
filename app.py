38a35ea0334b6465ce8b6c3b25b551ad421ff435
Zaldea5857 <16304@edu.szf.at>
2023-07-20 12:41:38 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/references')
+def references():
+    return render_template('references.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/jobs')
+def jobs():
+    return render_template('jobs.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
